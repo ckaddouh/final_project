9ab0ef88141f4b94676908a48fb29a9835182b51
ckaddouh <ckaddouh@gmail.com>
2020-05-29 09:47:03 -0400

    Adding more features

--- drawing.java
+++ drawing.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.layout.GridPane;
@@ -70,7 +72,11 @@ public class drawing extends Application {
             Button penBT = new Button("Pen");
             penBT.setOnAction( e -> {
                 gc.setStroke(cp.getValue());
+                gc.setLineWidth(slider.getValue());
             });
+
+            ObservableList<String> erasers = FXCollections.observableArrayList ("Eraser", "Select Erase");
+            
 
             Button eraseBT = new Button("Eraser");
             eraseBT.setOnAction(e -> {
