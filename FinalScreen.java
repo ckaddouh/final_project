55d4a6779b6b27cb1c951812885238c895ca45d8
dongjoo224 <60186548+dongjoo224@users.noreply.github.com>
2020-06-12 12:59:59 -0400

    lol cimmt!

--- FinalScreen.java
+++ FinalScreen.java
@@ -1,6 +1,9 @@
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class FinalScreen extends BorderPane {
 
@@ -11,7 +14,17 @@ public class FinalScreen extends BorderPane {
         this.mainApp = app;
         
         Label complete = new Label("Great job!");
+        complete.setFont(new Font("verdana", 24));
         setTop(complete);
         setAlignment(complete, Pos.CENTER);
+
+        ImageView imageView = new ImageView();
+        imageView.setImage(new Image("image/confetti.gif"));
+        setCenter(imageView);
+
+        Label thanks = new Label("          Thanks for playing! \nWe hope you enjoyed the game! :)");
+        thanks.setFont(new Font("verdana", 18));
+        setBottom(thanks);
+        setAlignment(thanks, Pos.CENTER);
     }
 }
