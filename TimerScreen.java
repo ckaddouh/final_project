da47b8d16083d10f8673b1b4d21071b81ca168ce
dongjoo224 <60186548+dongjoo224@users.noreply.github.com>
2020-06-13 23:35:22 -0400

    sdf

--- TimerScreen.java
+++ TimerScreen.java
@@ -6,27 +6,12 @@ import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-<<<<<<< HEAD
-=======
 import javafx.scene.layout.BorderPane;
->>>>>>> 6ea45953e40307ef7117697aa07d30825cdde9da
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.util.Duration;
 
-<<<<<<< HEAD
-public class TimerScreen extends Application {
-
-    private final Integer startTime = 10;
-    private Integer seconds = startTime;
-    private Label label;
-
-    @Override
-    public void start (Stage windows) throws Exception {
-
-
-=======
 public class TimerScreen extends BorderPane {
 
     private MainApp mainApp;
@@ -38,19 +23,11 @@ public class TimerScreen extends BorderPane {
         super();
         this.mainApp = app;
     
->>>>>>> 6ea45953e40307ef7117697aa07d30825cdde9da
         label = new Label();
         label.setTextFill(Color.ORANGE);
         label.setFont(Font.font(20));
         HBox layout= new HBox(5);
         layout.getChildren().add(label);
-<<<<<<< HEAD
-        doTime();
-
-        windows.setScene(new Scene(layout, 300, 70, Color.BLACK));
-        windows.setTitle("Countdown Timer");
-        windows.show();
-=======
 
         setCenter(layout);
         doTime();
@@ -58,7 +35,6 @@ public class TimerScreen extends BorderPane {
         // setScene(new Scene(layout, 300, 70, Color.BLACK));
         // setTitle("Countdown Timer");
         // show();
->>>>>>> 6ea45953e40307ef7117697aa07d30825cdde9da
 
     }
 
@@ -87,14 +63,7 @@ public class TimerScreen extends BorderPane {
 
     }
 
-<<<<<<< HEAD
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-=======
     // public static void main(String[] args) {
     //     launch(args);
     // }
->>>>>>> 6ea45953e40307ef7117697aa07d30825cdde9da
 }
